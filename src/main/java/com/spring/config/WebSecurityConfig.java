9b8bfd56abd2e785dfed0f4b15f53717b634099d
amit <amit.goyal@vvdntech.in>
2019-06-14 11:36:09 +0530

    sdvf

--- src/main/java/com/spring/config/WebSecurityConfig.java
+++ src/main/java/com/spring/config/WebSecurityConfig.java
@@ -17,6 +17,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	
+    	
+    	
         // disable caching
         http.headers().cacheControl();
 
